0de0129b16db26829113b859a3b4261a7869c98c
ronit <uttkarshbartwal@gmail.com>
2024-02-08 11:21:30 +0530

    backend

---
+++ part2/src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import ReactDOM from 'react-dom/client'
+
+import axios from 'axios'
+
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
+const promise = axios.get('http://localhost:3001/notes')
+
+promise.then(response => {
+  console.log(response)
+})
+
+import App from './App'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App notes={notes} />
+)
+
+
+
