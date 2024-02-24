767b3bcfe80938b881f30448bcd8075409a3f81a
ronit <uttkarshbartwal@gmail.com>
2024-02-24 10:54:18 +0530

    fullstack2.1

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,40 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const Note = require('./models/note')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-app.use(express.static('dist'))
+
+
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors());
+//app.use(requestLogger) 
+
+
+
+//const password = process.argv[2]
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+//const url = `mongodb+srv://fullstack:${password}@cluster0.vovpj4p.mongodb.net/?retryWrites=true&w=majority`;
+
+
+//mongoose.set('strictQuery',false)
+//mongoose.connect(url)
+
+//const noteSchema = new mongoose.Schema({
+  //content: String,
+  //important: Boolean,
+//})
+
+ //Note = mongoose.model('Note', noteSchema)
+
+
+
+
+
+
 
 let notes = [
   {
@@ -29,17 +59,33 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+//app.get('/api/notes/:id', (request, response) => {
+  //const id = Number(request.params.id)
+  //const note = notes.find(note => note.id === id)
   
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
+  //if (note) {
+    //response.json(note)
+  //} else {
+   // response.status(404).end()
+  //}
+//})
+
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+    })
+
+
 
 //app.get('/api/notes/:id', (request, response) => {
   //const id = Number(request.params.id)
@@ -53,14 +99,22 @@ app.get('/api/notes/:id', (request, response) => {
   //response.json(note)
 
 
+//app.get('/api/notes', (request, response) => {
+  //response.json(notes)
+//})
+
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -78,17 +132,55 @@ app.post('/api/notes', (request, response) => {
       error: 'content missing' 
     })
   }
+ })
+
+ app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
   const note = {
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
+
+app.prependOnceListener('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if(body.content === undefined) return response.status(404).json({error: 'missing content'})
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' });
+    }
+  
+    next(error);
+  };
+  
+  app.use(errorHandler);
 
 
 //app.post('/api/notes', (request, response) => {
@@ -97,7 +189,7 @@ app.post('/api/notes', (request, response) => {
   //response.json(note)
 //})
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
